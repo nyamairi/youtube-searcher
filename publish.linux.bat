4bc77e5ed587956ecdd68de2c27bf1b6a05bb53b
nyamairi <n.yamairi@gmail.com>
2021-10-23 21:38:43 +0900

    環境用のデプロイスクリプトを追加する #3
    (mac については動作確認していません :pray: )

---
+++ publish.linux.bat
@@ -0,0 +1,1 @@
+dotnet publish -c Release -r linux-x64 -o .\publish\linux-x64 .\YouTubeSearcher\YouTubeSearcher.csproj
---
+++ publish.osx.bat
@@ -0,0 +1,1 @@
+dotnet publish -c Release -r osx-x64 -o .\publish\osx-x64 .\YouTubeSearcher\YouTubeSearcher.csproj
